9f807105abe083633864268b7ad1477bfdbbd5ff
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2024-05-03 11:11:18 +0300

    update: add header guards.

--- SunmiPrinterTaskDemo/SunmiPrinterTaskDemo-Bridging-Header.h
+++ SunmiPrinterTaskDemo/SunmiPrinterTaskDemo-Bridging-Header.h
@@ -2,4 +2,9 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#ifndef SunmiPrinterTaskDemo_Bridging_Header_h
+#define SunmiPrinterTaskDemo_Bridging_Header_h
+
 #import <SunmiPrinterSDK/SunmiPrinterSDK.h>
+
+#endif /* SunmiPrinterTaskDemo_Bridging_Header_h */
